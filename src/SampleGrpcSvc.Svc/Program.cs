98e22a0c9ce6b7e8bd762aaf4da546578b08ddf1
Leonardo Guilen <leonardo.guilen@db1.com.br>
2021-11-09 18:24:38 -0300

    chore: initial commit

---
+++ src/SampleGrpcSvc.Svc/Program.cs
@@ -0,0 +1,23 @@
+using SampleGrpcSvc.Svc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcHttpApi();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<GreeterService>();
+    endpoints.MapGrpcReflectionService();
+});
+
+app.Run();
